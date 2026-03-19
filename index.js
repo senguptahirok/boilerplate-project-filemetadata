188d836d85d461f2566c5adcb538b1c897d3e008
Hirok Sengupta <senguptahirok@gmail.com>
2026-03-19 15:04:22 +0530

    file upload logic

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+var upload = multer({dest: './boilerplate-project-filemetadata/uploads/'});
 require('dotenv').config()
 
 var app = express();
@@ -7,12 +9,15 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+let uploadMiddleware = upload.fields([{name: upfile, maxCount: 1}]);
+app.post('/api/fileanalyse', uploadMiddleware, function(req, res, next){
+  upfile = originalname; 
+  res.json({name: upfile, type: mimetype, size: size});
+});
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
