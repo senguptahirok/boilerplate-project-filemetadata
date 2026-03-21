b7220f5364c828a09dcd8b30f74d3eb3d781fecb
Hirok Sengupta <senguptahirok@gmail.com>
2026-03-21 14:14:29 +0530

    debugging

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var multer = require('multer');
-var upload = multer({dest: 'uploads/'});
+// var upload = multer({dest: 'uploads/'});
 var cors = require('cors');
 require('dotenv').config()
 
@@ -9,10 +9,11 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-let uploadMiddleware = upload.single('file');
-app.post('/api/fileanalyse', uploadMiddleware, function(req, res, next){
+//let uploadMiddleware = upload.single('file');
+app.post('/api/fileanalyse', multer.none(), function(req, res, next){
 //  res.json({name: upfile, type: File.mimetype, size: File.size, msg: "file uploaded successfully"});
-    res.send('file uploaded successfully');
+console.log('req.body = ' + req.body);
+res.send('file uploaded successfully : ' + req.body);
 });
 
 app.get('/', function (req, res) {
