72b8fb1398af9b7577f8aa5db320bbec03048f18
Hirok Sengupta <senguptahirok@gmail.com>
2026-04-12 19:01:53 +0530

    debugging response

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 var multer = require('multer');
-var upload = multer({dest: 'public/'});
+var upload = multer({dest: './public/uploads/'});
 require('dotenv').config()
 
 var app = express(); 
@@ -10,7 +10,7 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
-   res.json({'body': req.body, 'file': req.file});
+   res.json({'name': req.file.originalname, 'type': req.file.mimetype, 'size': req.file.size});
 });
 
 app.get('/', function (req, res) {
