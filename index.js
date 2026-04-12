593fc559cf9c66858a9680f3fec2084426ce256e
Hirok Sengupta <senguptahirok@gmail.com>
2026-04-12 17:52:22 +0530

    debugging the response

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
-   res.json({'name': req.file.originalname, 'type': req.file.mimetype, 'size': req.file.size});
+   res.json({'name': req.file.filename, 'type': req.file.mimetype, 'size': req.file.size, 'destination': req.file.destination});
 });
 
 app.get('/', function (req, res) {
